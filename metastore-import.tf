c23310f16f1c8f455b6791dd3fc5d2b78254193b
Nadezhda-Subotka <144116230+Nadezhda-Subotka@users.noreply.github.com>
2024-12-26 15:48:29 +0300

    YCDOCIO-4211: Removed static keys from TF file (#5)

--- metastore-import.tf
+++ metastore-import.tf
@@ -147,17 +147,8 @@ resource "yandex_resourcemanager_folder_iam_binding" "storage-admin" {
   members   = ["serviceAccount:${yandex_iam_service_account.dataproc-sa.id}"]
 }
 
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  description        = "Static access key for Object Storage"
-  service_account_id = yandex_iam_service_account.dataproc-sa.id
-}
-
-# Use the key to create a bucket and grant permission to the service account in order to read from the bucket and write to it
 resource "yandex_storage_bucket" "dataproc-bucket" {
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
   bucket     = local.bucket_name
-
   grant {
     id = yandex_iam_service_account.dataproc-sa.id
     type        = "CanonicalUser"
